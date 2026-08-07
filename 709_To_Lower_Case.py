383c2bb7eac736e6a0a0ba15290944ac05253ce2
Pathan-Irfan8007 <pathanirfan8007@gmail.com>
2026-08-07 15:58:47 +0530

    Day 13

---
+++ 709_To_Lower_Case.py
@@ -0,0 +1,23 @@
+'''
+Q. Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+
+Example 1:
+Input: s = "Hello"
+Output: "hello"
+
+Example 2:
+Input: s = "here"
+Output: "here"
+
+Example 3:
+Input: s = "LOVELY"
+Output: "lovely"
+'''
+# Ans :
+
+def toLowerCase(s):
+    return s.lower()
+
+print(toLowerCase("Hello"))
+print(toLowerCase("here"))
+print(toLowerCase("LOVELY"))
