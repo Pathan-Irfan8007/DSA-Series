65420672da9f42821b499529847d83df78d622f3
Pathan-Irfan8007 <pathanirfan8007@gmail.com>
2026-08-17 20:13:05 +0530

    Day 17

---
+++ 144_BT_Preorder_Traversal.py
@@ -0,0 +1,32 @@
+'''
+Q. Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+Example 1:
+Input: root = [1,null,2,3]
+Output: [1,2,3]
+
+Example 2:
+Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+Output: [1,2,4,5,6,7,3,8,9]
+
+Example 3:
+Input: root = []
+Output: []
+
+Example 4:
+Input: root = [1]
+Output: [1]
+'''
+# Ans :
+
+def preorderTraversal(root):
+    result = []
+
+    def preorder(node):
+        if node:
+            result.append(node.val)
+            preorder(node.left)
+            preorder(node.right)
+
+    preorder(root)
+    return result
