e681a36525954a9d6c6f9efed182d0aa59f9a001
Pathan-Irfan8007 <pathanirfan8007@gmail.com>
2026-08-31 14:47:06 +0530

    Day 25

---
+++ 1572_Matrix_Diagonal_Sum.py
@@ -0,0 +1,51 @@
+'''
+Q. Given a square matrix mat, return the sum of the matrix diagonals.
+Only include the sum of all the elements on the primary diagonal and all the elements on the secondary diagonal that are not part of the primary diagonal.
+
+Example 1:
+Input: mat = [[1,2,3],
+              [4,5,6],
+              [7,8,9]]
+Output: 25
+Explanation: Diagonals sum: 1 + 5 + 9 + 3 + 7 = 25
+Notice that element mat[1][1] = 5 is counted only once.
+
+Example 2:
+Input: mat = [[1,1,1,1],
+              [1,1,1,1],
+              [1,1,1,1],
+              [1,1,1,1]]
+Output: 8
+
+Example 3:
+Input: mat = [[5]]
+Output: 5
+'''
+# And :
+
+def diagonalSum(mat):
+    rows = len(mat)
+    cols = len(mat[0])
+
+    result = 0
+
+    for i in range(rows):
+        result += mat[i][i]
+
+    n = cols - 1
+    i = 0
+
+    while i < rows:
+        if i == n:
+            i += 1
+        else:
+            result += mat[i][n]
+            i += 1
+        n -= 1
+
+    return result
+
+print(diagonalSum([[1,2,3],[4,5,6],[7,8,9]])) 
+print(diagonalSum([[1,1,1,1],[1,1,1,1],[1,1,1,1],[1,1,1,1]])) 
+print(diagonalSum([[5]])) 
+
